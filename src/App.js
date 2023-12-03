7d3de9fd4325e1595650ebe00b0526ec78dd3bb3
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-12-03 16:38:52 +0600

    react router setup is complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Home from './pages/Home';
 import Video from './pages/Video';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/Footer';
 
 function App() {
     return (
-        <div>
-            {/* <Home /> */}
-            <Video />
-        </div>
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/videos/:videoId" element={<Video />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
     );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import Navbar from '../components/navbar/Navbar';
 import VideoGrid from '../components/grid/VideoGrid';
 import Pagination from '../components/ui/Pagination';
-import Footer from '../components/Footer';
 import Tags from '../components/tags/Tags';
 
 const Home = () => {
     return (
         <>
-            <Navbar />
             <Tags />
             <VideoGrid />
             <Pagination />
-            <Footer />
         </>
     );
 };
