1bdf26b9451a443b61cb87a3d48a156bc3091a8d
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-12-06 00:14:26 +0600

    video feature is completed

--- src/components/description/Player.js
+++ src/components/description/Player.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Player = () => {
+const Player = ({ title, link }) => {
     return (
         <div>
-            <iframe width="100%" class="aspect-video" src="https://www.youtube-nocookie.com/embed/6O4s7v28nlw" title="Some video title" frameBorder="" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullscreen></iframe>
+            <iframe width="100%" className="aspect-video" src={link} title={title} frameBorder="" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
     );
 };
